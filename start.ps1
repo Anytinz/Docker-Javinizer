338ca475946d1f2aefa2e2775a6be131b833a76d
Anytinz <anytinz.ovo@gmail.com>
2020-02-11 20:45:10 +0800

    Fix the problem that PoshRSJob is not installed

--- start.ps1
+++ start.ps1
@@ -1,16 +1,17 @@
-if( Get-Module -ListAvailable PoshRSJob ){
+if( Get-Module -ListAvailable PoshRSJob ) {
+    break
+}
+else {
     Install-Module -Force PoshRSJob
 }
 
 Import-Module /Javinizer/Javinizer.psm1
 $env:PSModulePath
 
-for( $i=1 ; $i -gt 0 ; $i++ )
-{   
-    if( $Enable_Emby_Actors ){
+for( $i=1 ; $i -gt 0 ; $i++ ) {   
+    if( $SET_EMBY_ACTOR_THUMBS ) {
         Javinizer -SetEmbyActorThumbs
     }
     Javinizer -Apply -Multi -Path /jav -DestinationPath /jav
     Start-Sleep -s 300
-
 }
